458f3c37f2bfa73a7c0b8e29f11fc52020f0a6cd
aneelameham <50913897+aneelameham@users.noreply.github.com>
2021-07-09 17:11:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './Careerlabs.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          CAREERLABS PVT LTD 
+        </p>
+        <a
+          className="App-link"
+          href="https://careerwizard.thecareerlabs.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit Us
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
